8b8d9bc2028317da594510efcf6e77790703668e
Alex <fuckyobitch@Alexs-MacBook-Pro.local>
2018-07-20 19:05:06 -0600

    still working towards user sign up

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -7,6 +7,9 @@ router.get('/', (req, res) => {
   res.render('../views/userViews/index.ejs');
 });
 
+router.get('/register', (req, res) => {
+  res.render('../views/userViews/new.ejs');
+});
 
 
 
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
 
 require('./db/db');
 
@@ -9,15 +12,23 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 
-
+const User = require('./models/users');
 const userRoutes = require('./controllers/userRoutes');
 
-app.use('/login', userRoutes);
+app.use('/piggybank', userRoutes);
 
-app.listen(3000, (err) => {
-  if(err){
-    console.log(err, 'error in listen');
-  }else {
+app.use(require('express-session')({
+  secret: 'fuck yo bitch slutty',
+  resave: false,
+  saveUnintialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.listen(3000, () => {
     console.log('i am watching....')
-  }
 });

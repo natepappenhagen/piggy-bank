7d3f0c5ccc4730db8c9c06b25329ff525e0352db
Alex <fuckyobitch@Alexs-MacBook-Pro.local>
2018-07-22 23:14:54 -0600

    user registration works

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,17 +1,60 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+//passport set up
+router.use(require('express-session')({
+  secret: 'fuck yo bitch slutty',
+  resave: false,
+  saveUnintialized: false
+}));
+router.use(passport.initialize());
+router.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-//index route
+
+//========================================
+//             Routes
+
+
+//login or register route
 router.get('/', (req, res) => {
   res.render('../views/userViews/index.ejs');
 });
 
+//add new users
 router.get('/register', (req, res) => {
   res.render('../views/userViews/new.ejs');
 });
 
+router.post('/register', (req, res) => {
+  let newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, (err, user) => {
+    if(err){
+      console.log(newUser);
+      console.log(req.body);
+      console.log(req.body.password);
+      console.log(user);
+      console.log(err, 'err in create new user');
+      return res.render('../views/userViews/new.ejs')
+    } else {
+      passport.authenticate('local')(req, res, (err) => {
+        if(err){
+          console.log(err, 'error in authenticate');
+        } else {
+          res.render('../views/userViews/show.ejs')
+        }
+      });
+    }
+  });
+});
 
+//edit users
+
+//delete users
 
 
 
--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const passport = require('passport');
-const LocalStrategy = require('passport-local');
+
 
 
 require('./db/db');
@@ -15,18 +14,14 @@ app.use(express.static('public'));
 const User = require('./models/users');
 const userRoutes = require('./controllers/userRoutes');
 
+
+
+
+
+
 app.use('/piggybank', userRoutes);
 
-app.use(require('express-session')({
-  secret: 'fuck yo bitch slutty',
-  resave: false,
-  saveUnintialized: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new LocalStrategy(User.authenticate()));
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
 
 
 app.listen(3000, () => {
